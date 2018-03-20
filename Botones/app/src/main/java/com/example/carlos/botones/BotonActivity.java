69825acb245b5ca73cf147d120d5f964e70d1dc0
Carlos Gijon <carlosgijonfernandez@gmail.com>
2018-03-20 13:21:13 +0100

    Cambios en botones: ToggleButton

--- Botones/app/src/main/java/com/example/carlos/botones/BotonActivity.java
+++ Botones/app/src/main/java/com/example/carlos/botones/BotonActivity.java
@@ -2,6 +2,8 @@ package com.example.carlos.botones;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ToggleButton;
 
 public class BotonActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class BotonActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_boton);
+    }
+
+    public void onToggleClicked(View vista) {
+        // ¿Esta On?
+        boolean encendido = ((ToggleButton)vista).isChecked();
+        if(encendido) {
+            // Vibrar ON
+        }
+        else {
+            // Vibrar OFF
+        }
     }
 }
