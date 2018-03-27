5806f686f2ada7d081f067d4b244aacc370aa6a4
Carlos Gijon <carlosgijonfernandez@gmail.com>
2018-03-27 13:59:18 +0200

    Nueva Subida: TextViews

---
+++ TextViews/app/src/main/java/com/example/carlos/textviews/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.carlos.textviews;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button BtnHaceCambios = findViewById(R.id.btnHacerCambios);
+        EditText TxtEditable = findViewById(R.id.txtEditable);
+
+        BtnHaceCambios.setOnClickListener(
+                (v) -> {
+                    // Se referencia el TextView
+                    final TextView lblEtiqueta = findViewById(R.id.LblEtiqueta);
+
+                    // almacenara el texto que haya en lblEtiqueta
+                    String texto = TxtEditable.getText().toString();
+
+                    // En lblEtiqueta se va a poner el String que contenga la variable texto
+                    lblEtiqueta.setText(texto);
+
+                    // Se va a cambiar el color de fondo de lblEtiqueta
+                    lblEtiqueta.setBackgroundColor(Color.BLUE);
+                }
+        );
+
+    }
+}
