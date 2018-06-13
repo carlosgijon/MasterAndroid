aa015a1cc85fb5f7574f5882f44e7e07f53adcfe
carlosgijon <carlosgijonfernandez@gmail.com>
2018-06-13 13:42:34 +0200

    Nueva subida: Ejemplo 3 menús

--- ProbandoMenus/app/src/main/java/com/example/carlos/probandomenus/MainActivity.java
+++ ProbandoMenus/app/src/main/java/com/example/carlos/probandomenus/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.carlos.probandomenus;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,11 +15,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Hacer un menu contextual con la etiqueta hello world!!!
+
+        // Declaramos el textView
+        TextView texto = (TextView)findViewById(R.id.tv_hello);
+
+        //Declaramos el método para hacer el menú contextual
+        registerForContextMenu(texto);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
         return true;
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        // Declaramos un menuInflater para que se vea el context menu
+        MenuInflater inflater = getMenuInflater();
+
+        // Mostrar el menu
+        inflater.inflate(R.menu.contextual, menu);
     }
 }
