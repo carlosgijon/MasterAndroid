ea0258c9878cb227374b44b82109933d39eaecda
carlosgijon <carlosgijonfernandez@gmail.com>
2018-06-20 13:14:16 +0200

    Nueva subida: Ejemplo 3 menús

--- ProbandoMenus/app/src/main/java/com/example/carlos/probandomenus/MainActivity.java
+++ ProbandoMenus/app/src/main/java/com/example/carlos/probandomenus/MainActivity.java
@@ -6,10 +6,14 @@ import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Creamos una variable global para controlar cuando se bloquean las opciones
+    private boolean contenido_bloqueado = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +27,34 @@ public class MainActivity extends AppCompatActivity {
 
         //Declaramos el método para hacer el menú contextual
         registerForContextMenu(texto);
+
+        // Asignamos el listener para el boton
+        final Button btn_desbloqueo = (Button)findViewById(R.id.btn_opciones);
+
+        // Le añadimos el listener al boton y su funcionalidad
+        btn_desbloqueo.setOnClickListener((view) -> {
+            contenido_bloqueado = true;
+            btn_desbloqueo.setEnabled(false);
+        });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu, menu);
+        getMenuInflater().inflate(R.menu.menu_agrupado, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        menu.setGroupEnabled(R.id.grupo, false);
+        // menu.setGroupVisible(R.id.grupo, false);
+
+        if(contenido_bloqueado) {
+            menu.add(0,5, Menu.NONE, "Opcion desbloqueada");
+            contenido_bloqueado = false;
+        }
+
+
         return true;
     }
 
